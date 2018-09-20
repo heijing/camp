483362248275235d79f9a9cd52431061358e2f36
dal131 <vickyleewhy@hotmail.com>
2018-09-20 18:34:34 +0000

    Add mongo lab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+//mongoose.connect("mongodb://localhost/yelp_camp_v9");
+mongoose.connect("mongodb://dal131:641107dandanyang@ds211083.mlab.com:11083/yelpcampdal131")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
